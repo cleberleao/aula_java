0dc9ba2d48530f5fd553188bcd224a97f8d3db2c
CLEBER JOSE LEAO DE ALMEIDA <cleber.almeida@ulife.com.br>
2023-03-23 21:23:47 -0300

    exercicio  92

---
+++ Main.java
@@ -0,0 +1,25 @@
+//Entrar com um número e imprimi-lo caso seja
+// maior que 20
+public class Main {
+    public static void main(String[] args) {
+        double num = 10;
+        if(num > 20){
+            System.out.println("O numero é maor que 20: "+num);
+        }
+        else if(num <= 20){
+            System.out.println("O número é menor que 20: "+num);
+        }
+    }
+}
+
+
+//        int num1 = 5;
+//        int num2 = 7;
+//        int soma;
+//        String nome = "Cleber ";
+//        String sobrenome = "Leão";
+//        soma = num1+num2;
+//        String nomeSobrenome = nome+sobrenome;
+//        System.out.println("soma de num1 + num2 é = " + nomeSobrenome);
+
+
