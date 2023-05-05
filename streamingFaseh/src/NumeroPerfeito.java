0b9ffb6f0c7d356256d9c5e7ba8453b8d3bc2e5d
cleberleao <cleberleao_14@hotmail.com>
2023-05-04 21:39:47 -0300

    update questao 10

---
+++ streamingFaseh/src/NumeroPerfeito.java
@@ -0,0 +1,25 @@
+//Faça uma classe com um método
+// que será usado em sistema
+//  que irá verificar se um valor é
+//  perfeito ou não. Um valor é dito
+//  perfeito quando ele é igual a soma
+//  dos seus divisores excetuando ele
+//  próprio. (Ex: 6 é perfeito,
+//  6 = 1 + 2 + 3, que são seus
+//  divisores). O método deve retornar
+//  um valor booleano.
+
+public class NumeroPerfeito {
+    private int numero;
+
+    public boolean numeroPerfeito(int numero){
+        this.numero = numero;
+        int perfeito ;
+        if(this.numero == 1){
+            return false;
+        } else if (this.numero > 2) {
+
+        }
+
+    }
+}
